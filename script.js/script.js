8d28aadc7007a633f0647e93ecde7563efa85e4e
isaiasfunes <140356099+isaiasfunes@users.noreply.github.com>
2023-09-01 19:43:31 -0300

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,11 @@
+var nombre = "Isaias";
+var apellido = "Funes";
+
+console.log(nombre + " " + apellido);
+
+var numUno = 7; 
+var numDos = 2;
+var suma = (numUno + numDos);
+console.log("La suma es: " + suma);
+var suma1 = Number(suma + (6));
+console.log("El resultado de la segunda suma es: " + suma1);
